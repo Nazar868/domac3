3fe436038646554c14c6833f632b9fa592fe50ff
Nazar868 <zagariynazar04@gmail.com>
2026-06-19 16:17:01 +0300

    Create database.py

---
+++ database.py
@@ -0,0 +1,9 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker, declarative_base
+
+DB_URL = "postgresql+psycopg2://postgres:mysecretpassword@localhost:5432/postgres"
+
+engine = create_engine(DB_URL)
+SessionLocal = sessionmaker(bind=engine)
+
+Base = declarative_base()
